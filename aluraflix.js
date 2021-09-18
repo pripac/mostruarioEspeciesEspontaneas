efb3d59cae2889edbca9fbefd25994d037df40d3
pripac <pristudy96@gmail.com>
2021-09-18 18:57:23 -0300

    v_plantas.espontaneas2

--- aluraflix.js
+++ aluraflix.js
@@ -1,4 +1,7 @@
-var listaPlantas = ["http://gfb.global.ssl.fastly.net/wp-content/uploads/2016/07/15-Worthy-Benefits-of-Waterleaf-Talinum-triangulare.jpg", "https://www.portalsyngenta.com.br/images_v2/glossario-de-alvos-picao-branco%20%285%29.jpg "," http://luirig.altervista.org/cpm/albums/bot-hawaii32/normal_16231-Youngia-japonica.jpg" , "https://static.wixstatic.com/media/dd0291_7d6f08ba10280ec2c0c22ed639fe9b2b.jpg/v1/fill/w_401,h_299,al_c,q_80,usm_0.66_1.00_0.01/dd0291_7d6f08ba10280ec2c0c22ed639fe9b2b.webp"]
+var listaPlantas = ["http://gfb.global.ssl.fastly.net/wp-content/uploads/2016/07/15-Worthy-Benefits-of-Waterleaf-Talinum-triangulare.jpg",
+ "https://www.portalsyngenta.com.br/images_v2/glossario-de-alvos-picao-branco%20%285%29.jpg ",
+ "https://cnas-re.uog.edu/wp-content/uploads/2015/10/Youngiawhpl.jpg" , 
+ "https://static.wixstatic.com/media/dd0291_7d6f08ba10280ec2c0c22ed639fe9b2b.jpg/v1/fill/w_401,h_299,al_c,q_80,usm_0.66_1.00_0.01/dd0291_7d6f08ba10280ec2c0c22ed639fe9b2b.webp"]
 
 
 for (var indice = 0; indice < listaPlantas.length; indice++)
